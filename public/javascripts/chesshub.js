755ebae27f27a68a63f0daf2c36263f246c97ebc
jaaimino <jacobaimino@gmail.com>
2016-02-29 09:41:35 -0500

    Heroku test

--- public/javascripts/chesshub.js
+++ public/javascripts/chesshub.js
@@ -17,13 +17,13 @@ $( document ).ready(function() {
     }
 
     // socket used for real time games
-    var socket = io('/', { query: 'user=' + username });
+    var socket = io(window.location.hostname + '/', { query: 'user=' + username });
 
     //socket used to broadcast live games on tv page
-    var tvSocket = io('/tv');
+    var tvSocket = io(window.location.hostname + '/tv');
 
     // socket used to broadcast events to monitoring page
-    var monitorSocket = io('/monitor');
+    var monitorSocket = io(window.location.hostname + '/monitor');
 
     // Puzzle of the day: initialize a chess board with puzzle data
     if ($("#pod").length) {
@@ -254,7 +254,7 @@ $( document ).ready(function() {
          * When a new game is created, the game creator should wait for an opponent to join the game
          */
         socket.on('wait', function () {
-            var url = "/game/" + token + "/" + opponentSide;
+            var url = window.location.hostname + "/game/" + token + "/" + opponentSide;
             $('#gameUrl').html(url);
             $('#gameUrlPopup').modal({ // show modal popup to wait for opponent
                 keyboard: false,
@@ -419,7 +419,7 @@ $( document ).ready(function() {
         $( "#searchGameFormSubmit" ).on("click", function( event ) {
             $.ajax({
                 type: "POST",
-                url: "/search",
+                url: window.location.hostname + "/search",
                 data: {
                     white: $( "input[name$='white']" ).val(),
                     black: $( "input[name$='black']" ).val(),
