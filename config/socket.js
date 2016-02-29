c06949241901120175740a07aacaa2553bf4bb64
jaaimino <jacobaimino@gmail.com>
2016-02-28 20:36:24 -0500

    Another heroku test

--- config/socket.js
+++ config/socket.js
@@ -4,6 +4,12 @@ module.exports = function (server) {
 
     var chess =  require('chess.js');
 
+    //Heroku
+    io.configure(function () {  
+      io.set("transports", ["xhr-polling"]); 
+      io.set("polling duration", 10); 
+    });
+
     /*
      * live show of top rated game
      */
